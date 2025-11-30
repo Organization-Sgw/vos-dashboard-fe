eee5c9ba82c8f4e528e2958245a3313a5bc570e7
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2025-12-01 01:13:13 +0700

    fix api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4002/v1',
+  baseURL: 'http://http://103.166.30.38:4002/v1',
 })
 
 axiosInstance.interceptors.response.use(
